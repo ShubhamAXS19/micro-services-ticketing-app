463473169c2a136344d806187c30714e876f41cf
Shubham Vishwakarma <sv09734@gmail.com>
2023-10-11 23:32:03 +0530

    setup done of k8s cluster for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import { json } from "body-parser";
+const express  = require("express");
+const json = require( "body-parser");
 
 const app = express();
 app.use(json());
